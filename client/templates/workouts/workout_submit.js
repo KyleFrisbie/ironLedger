2bac7e05ce75d4b5dc84bd6c0a3815a80c12b4ac
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-04-15 22:22:10 -0600

    exercises in workouts collection

--- client/templates/workouts/workout_submit.js
+++ client/templates/workouts/workout_submit.js
@@ -1,29 +1,39 @@
 Template.workoutSubmit.helpers({
-  exercises: function() {
-    return Exercises.find();
+  // exercises: function() {
+  //   return Exercises.find();
+  // }
+  selectTwo: function () {
+    return {
+      placeholder: 'Add exercises',
+      tags: true,
+      tokenSeparators: [',', ' ']
+    }
+  },
+  relatedWorkoutDoc: function () {
+    return Workouts.findOne()
   }
 });
 
 Template.workoutSubmit.events({
-  'submit form': function (e) {
-    e.preventDefault();
-
-    var workout = {
-      name: $(e.target).find('[name=name]').val(),
-    };
-    
-    Meteor.call('workoutInsert', workout, function (error, result) {
-      if (error) {
-        return sAlert.error(error.reason);
-      }
-      
-      if (result.workoutExists) {
-        sAlert.error('This workout has already been posted');
-      }
-      
-      Router.go('workoutPage', {_id: result._id})
-    });
-  }
+  // 'submit form': function (e) {
+  //   e.preventDefault();
+  //
+  //   var workout = {
+  //     name: $(e.target).find('[name=name]').val(),
+  //   };
+  //
+  //   Meteor.call('workoutInsert', workout, function (error, result) {
+  //     if (error) {
+  //       return sAlert.error(error.reason);
+  //     }
+  //
+  //     if (result.workoutExists) {
+  //       sAlert.error('This workout has already been posted');
+  //     }
+  //
+  //     Router.go('workoutPage', {_id: result._id})
+  //   });
+  // }
 });
 
 Template.workoutSubmit.onCreated(function () {
