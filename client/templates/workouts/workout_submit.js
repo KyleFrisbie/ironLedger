48fe5b94503756af489c183b4304491cc96982c7
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-04-23 18:52:14 -0600

    customizing workout submit form

--- client/templates/workouts/workout_submit.js
+++ client/templates/workouts/workout_submit.js
@@ -7,7 +7,7 @@ function exerciseSelect() {
       text: exercise.name
     });
   });
-  $('#exercises tbody:last-child .exerciseNameSelect').select2({
+  $('.exercise').select2({
     data: data,
     tags: true,
     placeholder: "exercise name"
@@ -22,52 +22,115 @@ Template.workoutSubmit.helpers({
 
 Template.workoutSubmit.events({
   'click .removeExercise': function (e) {
-    $(('#exercise_' + e.currentTarget.id)).remove();
+    $(e.currentTarget).parent().parent().parent().remove();
   },
-  'select2:select #exerciseNameSelect': function () {
+  'select2:select .exerciseNameSelect': function () {
     var exerciseId = $('#exerciseNameSelect :selected').text();
     if (!(Exercises.findOne({name: exerciseId}))) {
-
+  
     }
   },
+  'click .removeSet': function(e) {
+    $(e.currentTarget).parent().parent().remove();
+  },
   'click #addExercise': function () {
-    var rows = $('#exercises > tbody > tr').length;
-    if (rows == 0) {
-      $('#exercises').show();
-    }
-    var newExercise = $('#exercises tbody:last-child').append('<tr id=\'exercise_' + rows + '\'>' +
-      '<td>' +
-      '    <select class="form-control exerciseNameSelect"></select>' +
-      '</td>' +
-      '<td>' +
-      '    <input type="number" class="form-control" placeholder="stress (lbs/time)">' +
-      '</td>' +
-      '<td>' +
-      '    <select class="form-control">' +
-      '        <option>lbs</option>' +
-      '        <option>kg</option>' +
-      '        <option>sec</option>' +
-      '    </select>' +
-      '</td>' +
-      '<td>' +
-      '    <input type="number" class="form-control" placeholder="strain (reps/distance)">' +
-      '</td>' +
-      '<td>' +
-      '    <select class="form-control">' +
-      '        <option>reps</option>' +
-      '        <option>m</option>' +
-      '        <option>miles</option>' +
-      '        <option>sec</option>' +
-      '    </select>' +
-      '</td>' +
-      '<td>' +
-      '    <input type="number" class="form-control" placeholder="rest time (s)">' +
-      '</td>' +
-      '<td>' +
-      '    <a href="#" id="' + rows + '" class="removeExercise btn btn-danger" role="button"><span class="glyphicon glyphicon-minus"></span></a>' +
-      '</td>' +
-      '</tr>');
+    var exercises = $('.exercise').length;
+    $('.exercises').append(
+      '<div id="exercise_' + exercises + '" class="well">' +
+      '<div class="row">' +
+      '<div class="col-md-10">' +
+      '  <select id="exercise_' + exercises + '" class="exercise form-control exerciseNameSelect"></select>' +
+      '</div>' +
+      '<div class="col-md-2">' +
+      '<button type="button" class="removeExercise btn btn-danger">Remove Exercise</button>' +
+      '</div>' +
+      '<div class="sets"><br/>' +
+      '    <label for="exercise_sets">Sets: </label>' +
+      '    <table class="table table-hover" id="exercise' + exercises + '_sets">' +
+      '        <thead>' +
+      '        <tr>' +
+      '            <th>Stress Quantity</th>' +
+      '            <th>Stress Unit</th>' +
+      '            <th>Strain Quantity</th>' +
+      '            <th>Strain Unit</th>' +
+      '            <th>Rest</th>' +
+      '            <th></th>' +
+      '        </tr>' +
+      '        </thead>' +
+      '        <tbody>' +
+      '        <tr>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="stress (lbs/time)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <select class="form-control">' +
+      '                    <option>lbs</option>' +
+      '                    <option>kg</option>' +
+      '                    <option>sec</option>' +
+      '                </select>' +
+      '            </td>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="strain (reps/distance)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <select class="form-control">' +
+      '                    <option>reps</option>' +
+      '                    <option>m</option>' +
+      '                    <option>miles</option>' +
+      '                    <option>sec</option>' +
+      '                </select>' +
+      '            </td>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="rest time (s)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <a href="#" class="removeSet btn btn-danger" role="button"><span' +
+      '                        class="glyphicon glyphicon-minus"></span></a>' +
+      '            </td>' +
+      '        </tr>' +
+      '        </tbody>' +
+      '    </table>' +
+      '    <button class="addSet btn btn-default btn-block" type="button" value="Input">Add Set' +
+      '    </button>' +
+      '</div>' +
+      '<hr>' +
+      '</div>'
+    );
     exerciseSelect();
+  },
+  'click .addSet': function (e) {
+    var setTable = $(e.currentTarget).parent().find('table');
+    var newExercise = setTable.find('tbody:last-child').append(
+      '<tr id=\'exerciseSet_\'>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="stress (lbs/time)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <select class="form-control">' +
+      '                    <option>lbs</option>' +
+      '                    <option>kg</option>' +
+      '                    <option>sec</option>' +
+      '                </select>' +
+      '            </td>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="strain (reps/distance)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <select class="form-control">' +
+      '                    <option>reps</option>' +
+      '                    <option>m</option>' +
+      '                    <option>miles</option>' +
+      '                    <option>sec</option>' +
+      '                </select>' +
+      '            </td>' +
+      '            <td>' +
+      '                <input type="number" class="form-control" placeholder="rest time (s)">' +
+      '            </td>' +
+      '            <td>' +
+      '                <a href="#" class="removeSet btn btn-danger" role="button"><span' +
+      '                        class="glyphicon glyphicon-minus"></span></a>' +
+      '            </td>' +
+      '        </tr>');
   },
   'submit form': function (e) {
     e.preventDefault();
